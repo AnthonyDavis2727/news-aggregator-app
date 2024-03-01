351cc5aca66f6feea0477e792321407aaef988fc
Slim <slim@personal.ht.home>
2024-02-29 23:02:32 -0500

    Added Styling and Began JS
    Styled the elements that already existed and began adding JS for news api

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-fetch('')
-    // .then(response => response.json())
-    // .then(data => console.log(data))
+const API_KEY = 'ceb0c7cfb3a74f2aa3f0ae8fea7d46fa';
+
+let currentPage = 1;
+let currentCategory = null;
+let currentKeyword = null;
+let isloading = false;
+let lastArticleCount = 0;
+
+function fetchNews(isSearching){
+    if (isloading) return;
+
+    isloading = true;
+    let url;
+    
+}
